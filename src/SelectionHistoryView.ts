56822f96423effecd040feb120e7061fa2659974
Arthur Carabott <arthurc@gmail.com>
2018-05-20 12:16:16 +0100

    4 buttons

--- src/SelectionHistoryView.ts
+++ src/SelectionHistoryView.ts
@@ -1,7 +1,8 @@
 import { Obs, observable } from "./Obs";
 import { SelectableView } from "./SelectableView";
 
-type historyItem = Map<SelectableView, boolean>;
+type HistoryItem = Map<SelectableView, boolean>;
+type HistoryPair = [SelectableView, boolean];
 
 export class SelectionHistoryView {
   public  el: HTMLElement;
@@ -12,9 +13,9 @@ export class SelectionHistoryView {
   protected parentEl: HTMLElement;
   protected _historyItemHeight: number;
   protected currentSelectionObs!: Obs;
-  protected hoveredHistoryItem?: historyItem;
+  protected hoveredHistoryItem?: HistoryItem;
   @observable
-  protected history!: historyItem[];
+  protected history!: HistoryItem[];
   protected historyObs!: Obs;
   protected _isPreviewing!: boolean;
   protected isCombiningPreviews: boolean;
@@ -29,23 +30,9 @@ export class SelectionHistoryView {
     this.history = [];
     this.isCombiningPreviews = false;
 
-    document.addEventListener("keydown", event => {
-      if (["ShiftLeft", "ShiftRight"].includes(event.code)) {
-        this.isCombiningPreviews = true;
-        this.updatePreview();
-      }
-    }, false);
-
-    document.addEventListener("keyup", event => {
-      if (["ShiftLeft", "ShiftRight"].includes(event.code) && !event.shiftKey) {
-        this.isCombiningPreviews = false;
-        this.updatePreview();
-      }
-    }, false);
-
     this.currentSelectionObs.subscribe((selectables: SelectableView[]) => {
       this.addToHistory(new Map(selectables.map(selectable => {
-        return <[SelectableView, boolean]>[selectable, selectable.selected];
+        return <HistoryPair>[selectable, selectable.selected];
       })));
     });
 
@@ -68,7 +55,7 @@ export class SelectionHistoryView {
       : this.parentEl.classList.remove("previewing");
   }
 
-  addToHistory(item: historyItem) {
+  addToHistory(item: HistoryItem) {
     const selectedStates = Array.from(item.values());
     if (!selectedStates.some(bool => bool)) { return; }
 
@@ -84,7 +71,7 @@ export class SelectionHistoryView {
     this.historyObs.notify();
   }
 
-  showPreview(item: historyItem) {
+  showPreview(item: HistoryItem) {
     item.forEach((selected, view) => view.preview = selected);
   }
 
@@ -104,14 +91,21 @@ export class SelectionHistoryView {
   render() {
     Array.from(this.el.children).forEach(child => this.el.removeChild(child));
     this.history.forEach((historyItem, i) => {
-      const button = document.createElement("div");
-      button.classList.add("history-item");
-      button.style.height = `${this.historyItemHeight}px`;
+      const view = document.createElement("div");
+      view.classList.add("history-item");
+      view.style.height = `${this.historyItemHeight}px`;
+      this.el.appendChild(view);
+
 
       const number = document.createElement("div");
       number.classList.add("history-number");
       number.textContent = `${this.history.length - i}`;
-      button.appendChild(number);
+      view.appendChild(number);
+
+
+      const preview = document.createElement("div");
+      view.appendChild(preview);
+      preview.classList.add("preview");
 
       historyItem.forEach((selected, view) => {
         const clone = view.cloneEl();
@@ -121,27 +115,140 @@ export class SelectionHistoryView {
                    : clone.classList.remove("selected");
         clone.classList.remove("hover");
 
-        button.appendChild(clone);
+        preview.appendChild(clone);
       });
 
-      button.addEventListener("click", () => {
+
+      const buttons = document.createElement("div");
+      buttons.classList.add("buttons");
+      view.appendChild(buttons);
+
+      // Solo
+      // -----------------------------------------------------------------------
+      const soloButton = document.createElement("div");
+      soloButton.classList.add("button");
+      soloButton.classList.add("solo");
+      soloButton.textContent = "S";
+      buttons.appendChild(soloButton);
+
+      soloButton.addEventListener("click", () => {
         historyItem.forEach((selected, view) => view.selected = selected);
         this.parentEl.classList.remove("previewing");
       }, false);
 
-      button.addEventListener("mouseenter", () => {
-        this.hoveredHistoryItem = historyItem;
-        this.updatePreview();
+      soloButton.addEventListener("mouseenter", () => {
+        this.showPreview(historyItem);
         this.isPreviewing = true;
       }, false);
 
-      button.addEventListener("mouseleave", () => {
-        this.hoveredHistoryItem = undefined;
+      soloButton.addEventListener("mouseleave", () => {
         this.hidePreview();
         this.isPreviewing = false;
       }, false);
 
-      this.el.appendChild(button);
+      // Add
+      // -----------------------------------------------------------------------
+
+      const addButton = document.createElement("div");
+      addButton.classList.add("button");
+      addButton.classList.add("add");
+      addButton.textContent = "+";
+      buttons.appendChild(addButton);
+
+      const getAddSelection = (): HistoryItem => {
+        const currentStateEntries = Array.from(this.history[0].entries());
+        const combinedState: HistoryItem = new Map(currentStateEntries.map(([view, selected]) => {
+          return <HistoryPair>[view, selected || historyItem.get(view)];
+        }));
+
+        return combinedState;
+      };
+
+      addButton.addEventListener("click", () => {
+        const addSelection = getAddSelection();
+        addSelection.forEach((selected, view) => view.selected = selected);
+        this.parentEl.classList.remove("previewing");
+      }, false);
+
+      addButton.addEventListener("mouseenter", () => {
+        this.showPreview(getAddSelection());
+        this.isPreviewing = true;
+      }, false);
+
+      addButton.addEventListener("mouseleave", () => {
+        this.hidePreview();
+        this.isPreviewing = false;
+      }, false);
+
+
+      // Subtract
+      // -----------------------------------------------------------------------
+      const subtractButton = document.createElement("div");
+      subtractButton.classList.add("button");
+      subtractButton.classList.add("subtract");
+      subtractButton.textContent = "-";
+      buttons.appendChild(subtractButton);
+
+      const getSubSelection = (): HistoryItem => {
+        const currentStateEntries = Array.from(this.history[0].entries());
+        const subtractedState: HistoryItem = new Map(currentStateEntries.map(([view, selected]) => {
+          return <HistoryPair>[view, selected && !historyItem.get(view)];
+        }));
+        return subtractedState;
+      };
+
+      subtractButton.addEventListener("click", () => {
+        const subSelection = getSubSelection();
+        subSelection.forEach((selected, view) => view.selected = selected);
+        this.parentEl.classList.remove("previewing");
+      }, false);
+
+      subtractButton.addEventListener("mouseenter", () => {
+        this.showPreview(getSubSelection());
+        this.isPreviewing = true;
+      }, false);
+
+      subtractButton.addEventListener("mouseleave", () => {
+        this.hidePreview();
+        this.isPreviewing = false;
+      }, false);
+
+
+      // Invert
+      // -----------------------------------------------------------------------
+
+      const invertButton = document.createElement("div");
+      invertButton.classList.add("button");
+      invertButton.classList.add("subtract");
+      invertButton.textContent = "I";
+      buttons.appendChild(invertButton);
+
+      const getInvertSelection = (): HistoryItem => {
+        const currentStateEntries = Array.from(this.history[0].entries());
+        const invertState: HistoryItem = new Map(currentStateEntries.map(([view, selected]) => {
+
+          return <HistoryPair>[view, historyItem.get(view) && !selected];
+        }));
+        return invertState;
+
+      };
+
+      invertButton.addEventListener("click", () => {
+        const invertSelection = getInvertSelection();
+        invertSelection.forEach((selected, view) => view.selected = selected);
+        this.parentEl.classList.remove("previewing");
+      }, false);
+
+      invertButton.addEventListener("mouseenter", () => {
+        this.showPreview(getInvertSelection());
+        this.isPreviewing = true;
+      }, false);
+
+      invertButton.addEventListener("mouseleave", () => {
+        this.hidePreview();
+        this.isPreviewing = false;
+      }, false);
+
     });
   }
 }
